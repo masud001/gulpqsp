f59008836a57869bbcae6edbc23524a7860909c8
masud001 <madhnagar@gmail.com>
2020-02-08 19:04:30 +0600

    initial packege is done!

---
+++ src/js/file1.js
@@ -0,0 +1,5 @@
+// comments 1
+console.log("janina 1");
+
+// comments 2
+console.log("janina 1");
---
+++ src/js/file2.js
@@ -0,0 +1,5 @@
+// comments 1 from file 2
+console.log("janina 1 from 2");
+
+// comments 2 from file 2
+console.log("janina 1 from 2");
